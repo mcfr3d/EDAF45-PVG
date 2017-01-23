f5ec9a5e9a1f236138ab4c22b00650dc3050ea1d
Måns Magnusson <exoji2e@gmail.com>
2017-01-23 13:21:28 +0100

    Story8: klar för acceptanstest

--- dasUberEnduroApplicationen/src/registration/Main.java
+++ dasUberEnduroApplicationen/src/registration/Main.java
@@ -11,31 +11,10 @@ import util.RegistrationWriter;
 
 public class Main {
 	public static void main(String [] args){
-		/*Race _80sh = new Race();
-		JFrame Schaufenster = new JFrame();
-			JButton knopf = new JButton("das registrern knopf");
-				knopf.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent denKnopfEvent) {
-						_80sh.addierenRennfahrer();
-						knopf.setText("ziffer af renners: " + _80sh.betrag());
-					}
-				});
-			JButton visenKnopfen = new JButton("registreren zi rennen");
-				visenKnopfen.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent denKnopfEvent) {
-						JOptionPane.showMessageDialog(null,  _80sh.toString());
-					}
-				});
-			
-		Schaufenster.add(knopf, BorderLayout.WEST);
-		Schaufenster.add(visenKnopfen, BorderLayout.EAST);
-		Schaufenster.pack();
-		Schaufenster.setSize(500, 500);
-		Schaufenster.setVisible(true);*/
+		
 		System.out.println("Ange registreringsfil");
 		Scanner sc = new Scanner(System.in);
 		String path = sc.next();
-		RegistrationWriter.write(path, "reg");
 		new Gui(path);
 		
 	}
