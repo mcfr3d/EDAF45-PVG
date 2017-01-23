9457425fd42eef7c4a8581a4f6b130987dcb5946
Fredrik Nyberg <htx10fny@student.lu.se>
2017-01-23 12:05:55 +0100

    Story 3.4: Modified main

--- dasUberEnduroApplicationen/src/resultMerge/Main.java
+++ dasUberEnduroApplicationen/src/resultMerge/Main.java
@@ -3,5 +3,15 @@ package resultMerge;
 public class Main {
 	public static void main(String[] args) {
 		
+		String startFilePath = "/path";
+		String finishFilePath = "/path";
+		String nameFilePath = "/path";
+		String resultFilePath = "/path";
+		
+		// TODO: Fix path
+		Database db = new Database();
+		IOReader.readTimes(startFilePath, finishFilePath, db);
+		IOReader.readNames(nameFilePath, db);
+		ResultWriter.write(resultFilePath, db);
 	}
 }
