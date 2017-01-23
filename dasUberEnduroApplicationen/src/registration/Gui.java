5ee41336a2829bd63c2a7255afbcc51a74d6e89f
Chokladmunk <dat15jh1@student.lu.se>
2017-01-23 11:40:16 +0100

    Add GUI

--- dasUberEnduroApplicationen/src/registration/Gui.java
+++ dasUberEnduroApplicationen/src/registration/Gui.java
@@ -1,11 +1,15 @@
 package registration;
 
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalTime;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -32,6 +36,7 @@ public class Gui extends JFrame {
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
 		textOutput = new JTextArea(10, 30);
+		textOutput.setFont(new Font("Helvetica", Font.PLAIN, 34));
 
 		panel.add(makeEntryPanel());
 		panel.add(textOutput);
@@ -41,12 +46,15 @@ public class Gui extends JFrame {
 	private JPanel makeEntryPanel() {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-
+		ActionListener listener = new RegistrationListener();
 		// Components
 		textEntry = new JTextField(10);
-
+		textEntry.addActionListener(listener);
+		textEntry.setFont(new Font("Helvetica", Font.PLAIN, 34));
+		textEntry.setMaximumSize(new Dimension(600,50));
 		JButton button = new JButton("Registrera");
-		button.addActionListener(new RegistrationListener());
+		button.addActionListener(listener);
+		button.setFont(new Font("Helvetica", Font.PLAIN, 34));
 
 		panel.add(textEntry);
 		panel.add(button);
@@ -57,8 +65,36 @@ public class Gui extends JFrame {
 	private class RegistrationListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			// TODO: Implement button function
-			System.out.println("Button pressed !");
+			String time = getCurrentTime();
+			String startNumber = textEntry.getText().trim();
+			if (correctInput(startNumber)) {
+				String outputText = startNumber + "; " + time + "\n" + textOutput.getText();
+				textOutput.setText(outputText);
+				
+			} else {
+				JOptionPane.showMessageDialog(null,
+					    "Felaktig input. Använd endast siffror.",
+					    "Felmeddelande",
+					    JOptionPane.ERROR_MESSAGE);
+			}
+			textEntry.setText("");
+		}
+
+		private boolean correctInput(String input) {
+			for (char c : input.toCharArray()) {
+				if (!Character.isDigit(c)) {
+					return false;
+				}
+			}
+			return input.length() != 0;
+		}
+
+		private String getCurrentTime() {
+			LocalTime currentTime = LocalTime.now();
+			String hour = String.format("%02d", currentTime.getHour());
+			String minute = String.format("%02d", currentTime.getMinute());
+			String second = String.format("%02d", currentTime.getSecond());
+			return hour + "." + minute + "." + second;
 		}
 	}
 }
