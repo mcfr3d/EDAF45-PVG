90eadb47cb599007070a0566a7baca3a9d31754f
Emil Wihlander <emil.wihlander@outlook.com>
2017-01-23 12:05:26 +0100

    3.3 färdig

--- dasUberEnduroApplicationen/test/util/ResultWriterTest.java
+++ dasUberEnduroApplicationen/test/util/ResultWriterTest.java
@@ -14,31 +14,69 @@ import resultMerge.Database;
 public class ResultWriterTest {
 
 	@Test
-	public void testEmptyDatabase() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testDatabaseOneEntry() throws IOException {
 		
 		Database db = new Database();
 		
 		String path = "output.txt";
 		db.setName(7,"Emil");
-		db.addStart(7,"00:01:00");
-		db.addFinish(7,"00:01:01");
+		db.addStart(7,"00.01.00");
+		db.addFinish(7,"00.01.01");
 
 		ResultWriter.write(path, db);
 		
 		List<String> ls = Files.readAllLines(Paths.get(path));
 		assertEquals(ls.size(),2);
 		assertEquals(ls.get(0), "StartNr; Namn; Totaltid; Starttid; Måltid");
-		assertEquals(ls.get(1), "7; Emil; --:--:--; 00:01:00; 00:01:01");
+		assertEquals(ls.get(1), "7; Emil; --.--.--; 00.01.00; 00.01.01");
+	}
+	@Test
+	public void testDatabaseMultipleEntry() throws IOException {
+		
+		Database db = new Database();
+		
+		String path = "output.txt";
+		
+		db.setName(7,"Emil");
+		db.addStart(7,"00.01.00");
+		db.addFinish(7,"00.01.01");
+
+		db.setName(9,"Jacob");
+		db.addStart(9,"00.20.00");
+		db.addFinish(9,"00.20.08");
+		
+		ResultWriter.write(path, db);
+		
+		List<String> ls = Files.readAllLines(Paths.get(path));
+		assertEquals(ls.size(),3);
+		assertEquals(ls.get(0), "StartNr; Namn; Totaltid; Starttid; Måltid");
+		assertEquals(ls.get(1), "7; Emil; --.--.--; 00.01.00; 00.01.01");
+		assertEquals(ls.get(2), "9; Jacob; --.--.--; 00.20.00; 00.20.08");
+	}
+	@Test
+	public void testDatabaseFaultyEntries() throws IOException {
+		
+		Database db = new Database();
+		
+		String path = "output.txt";
+		
+		db.setName(7,"Emil");
+		db.addStart(7,"00.01.00");
+		db.addFinish(7,"00.01.01");
+
+		db.setName(9,"Jacob");
+		db.addStart(9,"00.20.00");
+		db.addStart(9,"00.20.07");
+		db.addFinish(9,"00.20.08");
+		
+		ResultWriter.write(path, db);
+		
+		List<String> ls = Files.readAllLines(Paths.get(path));
+		assertEquals(ls.size(),3);
+		assertEquals(ls.get(0), "StartNr; Namn; Totaltid; Starttid; Måltid");
+		assertEquals(ls.get(1), "7; Emil; --.--.--; 00.01.00; 00.01.01");
+		assertEquals(ls.get(2), "9; Jacob; --.--.--; Flera Starttider?; 00.20.08");
 	}
 
-	@Test
-	public void testNoDatabase() {
-		ResultWriter.write("output.txt", null);
-		
-	}
+
 }
