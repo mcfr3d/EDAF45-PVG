59ca92bf7a2bfbe4c1c702fb89f67123e6cf283c
Emil Wihlander <emil.wihlander@outlook.com>
2017-01-23 14:23:59 +0100

    Story 8: skriver korrekt output till file nu!

--- dasUberEnduroApplicationen/src/registration/Main.java
+++ dasUberEnduroApplicationen/src/registration/Main.java
@@ -1,20 +1,14 @@
 package registration;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Scanner;
-
-import javax.swing.*;
-
-import util.RegistrationWriter;
 
 public class Main {
 	public static void main(String [] args){
 		
-		System.out.println("Ange registreringsfil");
+		System.out.println("Ange registreringsfil");		
 		Scanner sc = new Scanner(System.in);
 		String path = sc.next();
+		sc.close();
 		new Gui(path);
 		
 	}
