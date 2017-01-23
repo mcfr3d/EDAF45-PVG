5b4afb905463e6f93ca44e6b880a7aab785551c6
eriksoderberg <dat12eso@student.lth.se>
2017-01-23 11:35:53 +0100

    Minor changes to Racer

--- dasUberEnduroApplicationen/src/resultMerge/Racer.java
+++ dasUberEnduroApplicationen/src/resultMerge/Racer.java
@@ -1,15 +1,33 @@
 package resultMerge;
 
+import java.util.LinkedList;
+
 public class Racer {
-  private String starttime, finishtime, name;
+  private LinkedList<String> starttimes, finishtimes;
+  private String name;
   private int startnumber;
+  
   public Racer(int startnumber) {
     this.startnumber = startnumber;
   }
+  
   public void addStart(String starttime) {
-    this.starttime = starttime;
+    starttimes.add(starttime);
   }
+  
   public void addFinish(String finishtime) {
-    this.finishtime = finishtime;
+    finishtimes.add(finishtime);
   }
+  public String toString() {
+	  return ""+startnumber + "; --.--.--; " + starttimes.getFirst() + "; " + finishtimes.getFirst();
+  }
+  
+  public String getErrors() {
+	  StringBuilder sb = new StringBuilder();
+	 
+	  
+	  //append if 0 starts, 0 finish, >1 start, >1 finish
+	  return sb.toString();
+  }
+  
 }
