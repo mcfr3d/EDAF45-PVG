6c328e8712fe0213b7357b9ae7809977df4e9aeb
Måns Magnusson <exoji2e@gmail.com>
2017-01-30 09:36:23 +0100

    Story 9.1: Refactor Racer with RaceType interface

---
+++ dasUberEnduroApplicationen/src/resultMerge/RaceType.java
@@ -0,0 +1,9 @@
+package resultMerge;
+
+public interface RaceType {
+	public void addStart(String start);
+	public void addFinish(String finish);
+	public String genResult();
+	public String getStart();
+	public String getFinish();
+}
