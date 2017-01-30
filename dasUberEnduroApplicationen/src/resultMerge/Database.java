68e89afb70dfa0153275f1518da4c79cbb3bdbd6
Jacob <jcanback@gmail.com>
2017-01-30 10:51:25 +0100

    Story 10 färdig

--- dasUberEnduroApplicationen/src/resultMerge/Database.java
+++ dasUberEnduroApplicationen/src/resultMerge/Database.java
@@ -1,62 +1,70 @@
 package resultMerge;
+
 import java.util.HashMap;
 
 public class Database {
 	private HashMap<Integer, Racer> racers;
-	
+
 	private boolean massStart = false;
 	private String massStartTime;
-	
+
 	public Database() {
 		racers = new HashMap<>();
 	}
-	
-	//TODO: check that format of time is ok.
-	public void setMassStart(String time) {
-		massStart = true;
-		massStartTime = time;
-		for(Racer r: racers.values()) {
-			r.addStart(time);
+
+	// TODO: check that format of time is ok.
+	public boolean setMassStart(String time) {
+
+		boolean correctFormat = true;
+
+		if (correctFormat) {
+
+			massStart = true;
+			massStartTime = time;
+			for (Racer r : racers.values()) {
+				r.addStart(time);
+			}
 		}
+		return correctFormat;
 	}
-	
-	
+
 	private Racer getRacer(int driver) {
-				
-		if(racers.containsKey(driver)) return racers.get(driver);
-			
+
+		if (racers.containsKey(driver))
+			return racers.get(driver);
+
 		Racer r = new Racer(driver);
-		racers.put(driver,r);
-		
+		racers.put(driver, r);
+
 		return r;
 	}
-	
-	public void addStart(int driver, String time){
-		
+
+	public void addStart(int driver, String time) {
+
 		Racer r = getRacer(driver);
-		
+
 		r.addStart(time);
 	}
 
-	public void addFinish(int driver, String time){
+	public void addFinish(int driver, String time) {
 
 		Racer r = getRacer(driver);
-		if(massStart) 
-			r.addStart(massStartTime); 
+		if (massStart)
+			r.addStart(massStartTime);
 		r.addFinish(time);
 	}
 
-	public void setName(int driver,String name){
+	public void setName(int driver, String name) {
 
 		Racer r = getRacer(driver);
-		
+
 		r.setName(name);
 	}
-	
+
 	public HashMap<Integer, Racer> getRacers() {
 		return racers;
 	}
-	
+
 	public int size() {
 		return racers.size();
 	}
--- dasUberEnduroApplicationen/src/resultMerge/MassStartRaceGui.java
+++ dasUberEnduroApplicationen/src/resultMerge/MassStartRaceGui.java
@@ -1,12 +1,56 @@
 package resultMerge;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import util.ResultWriter;
 
 public class MassStartRaceGui extends RaceGui {
 
+	private class MassStartButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent eee) {
+
+			String s = JOptionPane.showInputDialog(null, "Tiden för massstart", "Skriv in masstart tid",
+					JOptionPane.QUESTION_MESSAGE);
+			
+			boolean b = db.setMassStart(s);
+			
+			if(!b) {
+				
+				JOptionPane.showMessageDialog(null, "Felaktig input.", "Felmeddelande",
+						JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+	}
+	
+	
+	protected void addMassStartButton() {
+		
+		JButton button = new JButton("Masstart");
+		
+		button.addActionListener(new MassStartButtonListener());
+		
+		this.add(button);
+	}	
+	
 	MassStartRaceGui(JFrame frame) {
 		super(frame,"Mass start race");
-		// TODO Auto-generated constructor stub
+				
+
+		this.addNameFileButton();
+		
+		addMassStartButton();
+		
+		this.addFinishFileButton();
+		this.addExportButton();
 	}
 
 }
