f935cbce9272b8074934a409983a208a1977ba7f
Måns Magnusson <exoji2e@gmail.com>
2017-01-30 11:30:03 +0100

    Story 9.2: add MultiLapRace + tests

--- dasUberEnduroApplicationen/src/resultMerge/MultiLapRace.java
+++ dasUberEnduroApplicationen/src/resultMerge/MultiLapRace.java
@@ -1,35 +1,76 @@
 package resultMerge;
 
-public class MultiLapRace implements RaceType {
+import java.util.LinkedList;
+import java.util.TreeSet;
 
+public class MultiLapRace implements RaceType {
+	public static int maxLaps = 0;
+	public static String raceTime = "";
+	private LinkedList<String> startTimes = new LinkedList<>();
+	private TreeSet<String> finishTimes = new TreeSet<>();
+	private String start = "";
+	private String finish = "";
 	@Override
 	public void addStart(String start) {
-		// TODO Auto-generated method stub
-
+		if(startTimes.isEmpty()) this.start = start;
+		startTimes.add(start);
+		maxLaps = Math.max(maxLaps, finishTimes.size());
 	}
 
 	@Override
 	public void addFinish(String finish) {
-		// TODO Auto-generated method stub
-
+		finishTimes.add(finish);
+		this.finish = finishTimes.last();
+		if(startTimes.size() > 0){
+			maxLaps = Math.max(maxLaps, finishTimes.size());
+		}
 	}
 
 	@Override
 	public String genResult() {
-		// TODO Auto-generated method stub
-		return null;
+		String[] res = new String[maxLaps*2 + 3];
+		for(int i = 0; i < res.length; i++){
+			res[i] = "";
+		}
+		res[maxLaps + 2] = startT();
+		int i = maxLaps + 3;
+		for(String s : finishTimes){
+			res[i++] = s;
+		}
+		res[0] = finishTimes.size() + "";
+		res[1] = TotalTimeCalculator.computeDifference(start, finish);
+		for(i = 0; i < finishTimes.size(); i++){
+			res[2+i] = TotalTimeCalculator.computeDifference(res[2 + maxLaps + i], res[3 + maxLaps + i]);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+				
+		for(i = 0; i < res.length; i++){
+			sb.append(res[i]).append("; ");
+		}
+		String out = sb.toString();
+		if(out.charAt(out.length() - 2) != ';') return out;
+		return out.substring(0, out.length() - 2);		
 	}
 
 	@Override
 	public String getStart() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return start;
 	}
 
 	@Override
 	public String getFinish() {
-		// TODO Auto-generated method stub
-		return null;
+		return finish;
+	}
+	private String startT() {
+		if(startTimes.size() > 0) return start;
+		else return "Start?";
+	}
+	
+	//Should only be used during testing
+	public void resetStatic() {
+		maxLaps = 0;
 	}
 
 }
--- dasUberEnduroApplicationen/test/resultMerge/OneLapRaceTest.java
+++ dasUberEnduroApplicationen/test/resultMerge/OneLapRaceTest.java
@@ -7,53 +7,50 @@ import org.junit.*;
 import resultMerge.Racer;
 
 public class OneLapRaceTest {
-	Racer temp;
+	OneLapRace temp;
 	
 	@Before
 	public void init() {
-		temp = new Racer(1);
-		temp.setName("person");
+		temp = new OneLapRace();
 	}
 
 	@Test
 	public void testNoStartTime() {
 		temp.addFinish("12.00.00");
-		assertEquals("1; person; --.--.--; Start?; 12.00.00; Omöjlig Totaltid?",temp.toString());
+		assertEquals("--.--.--; Start?; 12.00.00; Omöjlig Totaltid?",temp.genResult());
 	}
 
 	@Test
 	public void testNoFinishTime() {
 		temp.addStart("12.00.00");
-		assertEquals("1; person; --.--.--; 12.00.00; Slut?; Omöjlig Totaltid?",temp.toString());
+		assertEquals("--.--.--; 12.00.00; Slut?; Omöjlig Totaltid?",temp.genResult());
 	}
 
 	@Test
 	public void testMultipleFinishTimes() {
 		temp.addFinish("12.00.00");
 		temp.addFinish("12.15.00");
-		assertEquals("1; person; --.--.--; Start?; 12.00.00; Flera måltider? 12.15.00; Omöjlig Totaltid?", temp.toString());
+		assertEquals("--.--.--; Start?; 12.00.00; Flera måltider? 12.15.00; Omöjlig Totaltid?", temp.genResult());
 	}
 	
 	@Test
 	public void testMultipleStartTimes() {
 		temp.addStart("12.00.00");
 		temp.addStart("12.15.00");
-		assertEquals("1; person; --.--.--; 12.00.00; Slut?; Flera starttider? 12.15.00; Omöjlig Totaltid?", temp.toString());
+		assertEquals("--.--.--; 12.00.00; Slut?; Flera starttider? 12.15.00; Omöjlig Totaltid?", temp.genResult());
 	}
 
 	@Test
 	public void testImpossibleTotalTime() {
 		temp.addStart("12.00.00");
 		temp.addFinish("12.10.00");
-		System.out.println(temp.toString());
-		assertEquals("1; person; 00.10.00; 12.00.00; 12.10.00; Omöjlig Totaltid?", temp.toString());
+		assertEquals("00.10.00; 12.00.00; 12.10.00; Omöjlig Totaltid?", temp.genResult());
 	}
 	
 	@Test
 	public void testPossibleTotalTime() {
 		temp.addStart("12.00.00");
 		temp.addFinish("12.15.00");
-		System.out.println(temp.toString());
-		assertEquals("1; person; 00.15.00; 12.00.00; 12.15.00", temp.toString());
+		assertEquals("00.15.00; 12.00.00; 12.15.00", temp.genResult());
 	}
 }
