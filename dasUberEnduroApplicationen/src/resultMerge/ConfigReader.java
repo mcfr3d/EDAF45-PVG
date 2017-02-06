d386c21729de00c256358d27a6df8c96c2f99ff8
Jacob <jcanback@gmail.com>
2017-02-06 14:00:12 +0100

    Fixade tillbaka commandline inför release

--- dasUberEnduroApplicationen/src/resultMerge/ConfigReader.java
+++ dasUberEnduroApplicationen/src/resultMerge/ConfigReader.java
@@ -17,7 +17,8 @@ public class ConfigReader {
 
 	public static void readConfig(String configFilePath) throws Exception {
 
-		String outputFilePath = null;
+		
+		List<Output> outputs = new LinkedList<>();
 		String type = "maraton";
 		String massStartTime = null;
 		String nameFilePath = null;
@@ -30,9 +31,17 @@ public class ConfigReader {
 
 			JSONObject root = new JSONObject(new JSONTokener(reader));
 
-			outputFilePath = root.getString("output");
+
+			JSONArray jsonOutputs = root.getJSONArray("outputs");
+			
+
+            for(int i = 0; i < jsonOutputs.length(); ++i)
+            	outputs.add(new Output(jsonOutputs.getJSONObject(i)));
+			
+			
+			//outputFilePath = root.getString("output");
 			type = root.optString("race type", "maraton");
-			massStartTime = root.optString("mass start", null);
+			massStartTime = root.optString("group start", null);
 			nameFilePath = root.getString("name file");
 
 			JSONArray jsonStartFiles = root.getJSONArray("start files");
@@ -55,16 +64,22 @@ public class ConfigReader {
 			System.exit(0);
 		}
 
+		System.out.println("start filer: " + startFiles);
+		System.out.println("finish filer: " + finishFiles);
+		
 		Database db = new Database(massStartTime, type.equals("varvlopp"));
 
 		IOReader.readNames(nameFilePath, db);
-
+		
 		for (String startFile : startFiles)
 			IOReader.readStart(startFile, db);
 		for (String finishFile : finishFiles)
 			IOReader.readFinish(finishFile, db);
 
-		ResultWriter.write(outputFilePath, db);
+		for (Output output : outputs)
+			output.write(db);
+		
+	//	ResultWriter.write(outputFilePath, db);
 
 	}
 
--- dasUberEnduroApplicationen/src/resultMerge/Main.java
+++ dasUberEnduroApplicationen/src/resultMerge/Main.java
@@ -1,6 +1,5 @@
 package resultMerge;
 
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,18 +13,42 @@ public class Main {
 
 		try {
 
-			Getopt g = new Getopt(args, "c:", true);
-
 			int c;
+			String outputFile = null;
+			String type = "maraton";
+			String massStartTime = null;
+			String nameFile = null;
+			List<String> startFiles = new LinkedList<>();
+			List<String> finishFiles = new LinkedList<>();
 
-			String configFilePath = "config.json";
-			
+			Getopt g = new Getopt(args, "o:t:n:s:f:m:", true);
+
 			while ((c = g.getOption()) != -1) {
 
 				switch (c) {
 
-				case 'c':
-					configFilePath = g.getOptarg();
+				case 'o':
+					outputFile = g.getOptarg();
+					break;
+
+				case 'm':
+					massStartTime = g.getOptarg();
+					break;
+
+				case 's':
+					startFiles.add(g.getOptarg());
+					break;
+
+				case 'f':
+					finishFiles.add(g.getOptarg());
+					break;
+
+				case 't':
+					type = g.getOptarg();
+					break;
+
+				case 'n':
+					nameFile = g.getOptarg();
 					break;
 
 				default:
@@ -33,10 +56,26 @@ public class Main {
 				}
 			}
 
-			ConfigReader.readConfig(configFilePath);
-			
+			if (outputFile != null && nameFile != null) {
+
+				Database db = new Database(massStartTime, type.equals("varvlopp"));
+
+				IOReader.readNames(nameFile, db);
+
+				for (String startFile : startFiles)
+					IOReader.readStart(startFile, db);
+				for (String finishFile : finishFiles)
+					IOReader.readFinish(finishFile, db);
+
+				ResultWriter.write(outputFile,db);
+				
+			} else {
+				
+				System.out.println("Otillräcklig indata");
+			}
+
 		} catch (Exception e) {
-			
+
 			System.err.println(e);
 		}
 
