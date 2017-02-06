028ab2d54c037522fcf8e052dcf1c4c31b1bfb1c
Jacob <jcanback@gmail.com>
2017-02-06 11:09:25 +0100

    Story 25: Fixade mkjars.sh med dependency till json biblioteket

--- dasUberEnduroApplicationen/mkjars.sh
+++ dasUberEnduroApplicationen/mkjars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p bin
-javac -d bin src/**/*.java
+javac -d bin -cp ../json-20160810.jar src/**/*.java
 mkdir -p jar
 cd bin
 jar cfm ../jar/registration.jar ../registration.manifest registration/*.class util/RegistrationWriter.class
