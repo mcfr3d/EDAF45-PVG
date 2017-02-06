6c0a9ef7a237f62fbbbc1668b7122b52e214ac8a
Emil Wihlander <emil.wihlander@outlook.com>
2017-02-06 10:45:57 +0100

    removed zipping from mkjars.sh

--- dasUberEnduroApplicationen/mkjars.sh
+++ dasUberEnduroApplicationen/mkjars.sh
@@ -5,13 +5,3 @@ mkdir -p jar
 cd bin
 jar cfm ../jar/registration.jar ../registration.manifest registration/*.class util/RegistrationWriter.class
 jar cfm ../jar/resultMerge.jar ../resultMerge.manifest resultMerge/*.class util/*.class
-
-
-cd ../
-#pwd
-mkdir DasUberEnduroApplikationen
-cp jar/registration.jar DasUberEnduroApplikationen/dasUberAnmeldung.jar 
-cp jar/resultMerge.jar DasUberEnduroApplikationen/dasUberResultat.jar
-cp Manual.html DasUberEnduroApplikationen/dasUberHandbuch.html
-zip -r DasUberEnduroApplikationen.zip DasUberEnduroApplikationen/
-rm -rf DasUberEnduroApplikationen
