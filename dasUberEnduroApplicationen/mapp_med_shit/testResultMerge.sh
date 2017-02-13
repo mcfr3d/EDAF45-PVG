dcf559c8efd8b2d97a38d98bfc1732b55b4eccd8
Måns Magnusson <exoji2e@gmail.com>
2017-02-13 13:45:17 +0100

    more clean up

--- dasUberEnduroApplicationen/mapp_med_shit/testResultMerge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-java -jar ../jar/resultMerge.jar -o resultat2.txt -s starttider.txt -f maltider.txt -n namnfil.txt -t maraton
-diff resultat.txt resultat2.txt
-rm resultat2.txt
-java -jar ../jar/resultMerge.jar -o resultat3.txt -m 00.01.37 -f maltider.txt -n namnfil.txt -t varvlopp
-
-
